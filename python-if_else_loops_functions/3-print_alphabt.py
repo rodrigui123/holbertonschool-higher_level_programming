49a348d703c78cbf1539ca3d0e007648c88c9e65
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-08 15:13:42 -0300

    fixing

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(97, 123):
+    if i == 101:
+        continue
+    elif i == 113:
+        continue
     letter = chr(i)
-    var = letter[65:-71]
     print("{}".format(letter), end="")
