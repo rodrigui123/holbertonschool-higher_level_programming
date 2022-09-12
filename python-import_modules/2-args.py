56da0853756c02c5909f105c927556987f6e6079
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-11 22:09:45 -0300

    trying

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print("{} argument:".format(number - 1))
     else:
         print("{} arguments:".format(number - 1))
+
     for i in range(1, number):
         print("{}: {}".format(i, sys.argv[i]))
