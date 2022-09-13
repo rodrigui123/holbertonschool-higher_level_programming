52a6030534cb3f696af5b0135d267878fb676307
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-13 13:33:57 -0300

    trying

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,4 +2,6 @@
 def new_in_list(my_list, idx, element):
     if (idx < 0 or idx >= len(my_list)):
         return my_list
-    my_list.insert(idx, element)
+    newlist = my_list.copy()
+    newlist[idx] = element
+    return newlist
