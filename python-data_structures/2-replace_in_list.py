ba6b6b26a176ec4a96d47bea3cb9bceb17ca11e4
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-13 11:32:39 -0300

    fixing ifs

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
-    element = my_list[idx]
-    return element
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list
+    else
+        my_list[idx] = element
+        return element
