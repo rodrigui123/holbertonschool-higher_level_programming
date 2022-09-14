a83fe8d91948a519ceee294048e3f5c1af33def5
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-14 14:26:29 -0300

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence == "":
         return (0, None)
     firstchar = sentence[0]
     sentencelen = len(sentence)
