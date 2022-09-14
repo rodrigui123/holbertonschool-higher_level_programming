fb975a4f105fcabdd7ab69d0249dc1441f08c8e9
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-14 10:07:47 -0300

    fixing

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     newstring = ""
     for i in my_string:
-        if i != "c" or i != "C":
+        if i != 'c' or i != 'C':
             newstring += i
     return newstring
     
