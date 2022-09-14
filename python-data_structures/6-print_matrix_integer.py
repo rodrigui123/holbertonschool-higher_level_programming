d37fc002a47cc8c1d242d9cd5218a1563f6c6be5
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-14 11:05:26 -0300

    trying

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
+    if matrix is None:
+        print()
+        return
     for row in matrix:
         for column in row:
             if column != row[-1]:
