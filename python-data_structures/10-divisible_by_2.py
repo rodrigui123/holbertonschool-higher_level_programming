0ee6dca62352a0c4839007383486b0786451e30a
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-15 19:03:08 -0300

    task10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newList = my_list.copy()
+    for i in my_list:
+        if i % 2 == 0:
+            newList.append(True)
+        else
+            newList.append(False)
+    return newList
