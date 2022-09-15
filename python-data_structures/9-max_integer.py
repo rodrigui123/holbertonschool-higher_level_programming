712303f9194381d2819b8c9fdab4577dd9ac4502
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-15 18:36:20 -0300

    fixing

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     largestNum = my_list[0]
-    if my_list is []:
+    if my_list == []:
         return None
     for num in my_list:
         if num > largestNum:
