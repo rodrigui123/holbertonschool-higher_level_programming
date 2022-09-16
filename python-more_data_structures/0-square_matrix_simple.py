c1f20d82957dfd84e6219b39f424dc09fb2cfb06
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-16 10:41:52 -0300

    task0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if otherMatrix is None:
+        return None
+    otherMatrix = []
+    for i in range(0, len(matrix)):
+        squareNum = list(map(lambda x: x * x, matrix[i]))
+        otherMatrix.append(squareNum)
+    return otherMatrix
