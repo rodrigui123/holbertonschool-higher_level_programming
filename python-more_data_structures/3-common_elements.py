47eb67da0c13b41ff87e0050a0f25763c0eb5535
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-16 12:30:40 -0300

    trying

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2)
-    if set_1 is None and set_2 is None:
-        return
-    return (set_1 & set_2)
+def common_elements(set_1, set_2):
+    result = set_1.intersection(set_2)
+    return result
