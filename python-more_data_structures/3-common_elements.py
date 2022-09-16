041bb621ed7de0ebd47de6ee0ccbc83963b67eb5
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-16 12:12:19 -0300

    task3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 is None or set_2 is None:
+    if set_1 is None and set_2 is None:
         return None
     return set_1 and set_2
