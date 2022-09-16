c84d762199e88deee36feadde5ca252726fa6dbb
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-16 10:47:59 -0300

    task0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if otherMatrix is None:
-        return None
     otherMatrix = []
     for i in range(0, len(matrix)):
         squareNum = list(map(lambda x: x * x, matrix[i]))
