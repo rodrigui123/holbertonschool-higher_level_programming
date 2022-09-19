9c7884ada7896f69df347d7a482d27cf7cad3703
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-19 15:13:02 -0300

    trying

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        printed = ""
+        printed = 0
         for i in range(0, x):
             print(f"{my_list[i]}", end="")
             printed += 1
