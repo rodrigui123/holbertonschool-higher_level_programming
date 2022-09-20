5eab3c9a3b3ad155282770e6d12a46906732c247
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 12:04:17 -0300

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,4 +11,6 @@ class Square:
     def __is__valid__name(self, size):
         if size < 0:
             raise ValueError("size must be >= 0")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         return size
