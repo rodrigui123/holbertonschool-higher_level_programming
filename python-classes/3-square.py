49577557f10285715e100898dc2bd51eefceff55
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 14:39:01 -0300

    task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creating an empty class that defines a square"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """this is the empty class"""
+    def __init__(self, size=0):
+        """adding a private attribute """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        sizeArea = self.__size * self.__size
+        return (sizeArea)
