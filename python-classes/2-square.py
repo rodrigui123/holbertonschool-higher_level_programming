f8bc810b501a9f332743e5a90b108092bda0e300
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 13:52:20 -0300

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,13 +6,8 @@ class Square:
     """this is the empty class"""
     def __init__(self, size=0):
         """adding a private attribute """
-        self.__size = size
-
-
-    def __init__(self, size):
         if size < 0:
             raise ValueError("size must be >= 0")
-    def __init__(self, size):
         if type(size) != int:
             raise TypeError("size must be an integer")
-        return size
+        self.__size = size
