5b04376ddee56f426762121e14a522b0e6b0a644
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 14:43:02 -0300

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,6 +15,8 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+
     def area(self):
         self.area = self.__size * self.__size
-    return (self.area)
+        return (self.area)
