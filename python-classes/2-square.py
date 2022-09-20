c00af2236aca03783d8496101ba254bb40cd0e24
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 12:37:14 -0300

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,6 @@ class Square:
     def __init__(self, size=0):
         """adding a private attribute """
         self.__size = size
-
-    def __is__valid__name(self, size):
         if size < 0:
             raise ValueError("size must be >= 0")
         if type(size) != int:
