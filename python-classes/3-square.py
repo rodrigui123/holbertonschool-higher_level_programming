04d75f8df1e901b8393346ab1aa88418bbd66415
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 14:47:45 -0300

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creating an empty class that defines a square"""
 
+
 class Square:
     """this is the empty class"""
     def __init__(self, size=0):
@@ -11,6 +12,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
 
     def area(self):
         self.area = self.__size * self.__size
