433a759b60c67d25c027416df4667043b9c04f9f
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-20 11:54:03 -0300

    task2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creating an empty class that defines a square"""
+
+
+class Square:
+    """this is the empty class"""
+    def __init__(self, size=0):
+        """adding a private attribute """
+        self.__size = size
+
+    def __is__valid__name(self, size):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        return size
