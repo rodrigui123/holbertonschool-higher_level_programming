a2243489981ff68ea119af5510245064f06723e2
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-21 11:28:59 -0300

    task5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,7 +27,6 @@ class Square:
                 if i == self.__size -1:
                     print(end="")
             print("#" * self.__size)
-        print()
 
     def area(self):
         return (self.__size * self.__size)
