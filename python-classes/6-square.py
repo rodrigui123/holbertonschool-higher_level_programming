fe518cf93d7b238a6e26523b80a9e9c951e394eb
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-21 14:57:09 -0300

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """this is the empty class"""
     def __init__(self, size=0,position=(0, 0)):
         """adding a private attribute """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -34,21 +34,18 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def my_print(self):
+        if self.__size == 0:
+                    print()
+        for i in range(self.__position[1]):
+                print(" ")
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", end="")
+        if i == self.__size - 1:
+            print(end="")
+            print("#" * self.__size)
 
     def area(self):
         return (self.__size * self.__size)
 
-    def my_print(self):
-        if self.__size == 0:
-                    print()
-        for i in range(self.__size):
-            if i == self.__position[0]:
-                print(" ")
-        for i in range(self.__size):
-            for i in range(self.__size):
-                if i == self.__position[1]:
-                    print()
-            for i in range(self.__size):
-                if i == self.__size - 1:
-                    print(end="")
-            print("#" * self.__size)
