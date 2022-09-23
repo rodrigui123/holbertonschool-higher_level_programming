baa95533d1621ad45d3e3cdbd2b2c1f0c9674c5a
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 18:31:47 -0300

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """python interpreter"""
 
-
 def add_integer(a, b=98):
     """python adding 2 integers"""
-
 
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
