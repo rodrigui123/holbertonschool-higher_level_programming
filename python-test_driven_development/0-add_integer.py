97b3b61c30eb5c17671c14d881671b816be7bcde
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 17:48:23 -0300

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,9 +5,8 @@
 def add_integer(a, b=98):
     """python adding 2 integers"""
 
-    if a is None or a is not int or a is not float:
+    if a is not int or a is not float:
         raise TypeError("a must be an integer")
     if b is not int or b is not float:
          raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+    return (a + b)
