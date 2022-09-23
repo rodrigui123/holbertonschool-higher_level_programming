b474c34ce0ebe44663b806574a6033e09c1e1776
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 19:32:30 -0300

    task2matrixdivide

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """divides all elements of a matrix"""
 
-    if div is not int and div is not float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -16,9 +16,9 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         rowSize = len(row)
         sublist = []
-        list .append(sublist)
         for elements in row:
-            sublist .append(elements)
+            sublist.append(elements)
             if type(matrix) is not list and type(matrix) is not int or type(matrix) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        list.append(sublist)
     return list
