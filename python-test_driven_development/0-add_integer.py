52d813575a2a1a55dff5cb40142e18f0710313b8
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 17:54:58 -0300

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,4 +10,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int or type(b) is not float:
         raise TypeError("b must be an integer")
-    return (a + b)
+    sum = int(a) + int(b)
+    return sum
