62408c2bfe2fcd7bef6b6db605e6b45f964d56ef
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 21:30:41 -0300

    task4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,6 +5,12 @@
 def print_square(size):
     """prints a square with the character #"""
 
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
     for i in range(size):
         for i in range(size):
             if i == size -1:
