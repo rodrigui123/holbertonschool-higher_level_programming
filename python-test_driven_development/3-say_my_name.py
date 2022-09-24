a9fb0220a0750adadde49adc6ae6a4801881c378
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-23 21:09:25 -0300

    task3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""python interpreter"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that says my name"""
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    return (f"My name is {first_name} {last_name}")
