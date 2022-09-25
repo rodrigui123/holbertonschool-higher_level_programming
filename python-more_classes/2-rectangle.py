c37be33e68d5c4ede8811eec5d2c4e512132bc6f
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-25 17:00:39 -0300

    task2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -48,10 +48,10 @@ class Rectangle:
 
         self.__height = value
 
-        def area(self):
+    def area(self):
             return(self.__width * self.__height)
 
-        def perimeter(self):
+    def perimeter(self):
             if self.__width == 0 or self.__height == 0:
                 return 0
             return (self.__width * 2) + (self.__height * 2)
