387140ddb8037ff650092de70fe0f571b7e8d5e1
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-25 16:59:34 -0300

    task2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,11 +6,12 @@ class Rectangle:
     """empty class"""
 
     def __init__(self, width=0, height=0):
+        """On instantiation"""
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-         if type(height) != int:
+        if type(height) != int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
