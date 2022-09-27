43fcbd27895c35ce18d0e5a8ae21cb5db5c80f77
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-27 19:23:29 -0300

    task2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""pyhton interpreter"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is an instance of the specified class"""
+    return True if type(obj) is a_class else False
