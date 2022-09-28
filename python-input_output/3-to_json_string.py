3e603e4b2eea2ea091cf5c063e86d6a8da56ac58
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-28 16:32:14 -0300

    task3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """pyhton interpreter"""
+import json
 
+
+def to_json_string(my_obj):
+    """function that returns the JSON
+    representation of an object (string)"""
+    return json.dump(my_obj)
