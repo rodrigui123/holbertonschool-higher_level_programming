0619a5844960a60cce61a00a616686f202b542f7
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-28 09:30:40 -0300

    task7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,6 +10,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError("age must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("age must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
