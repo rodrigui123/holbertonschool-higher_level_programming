544a1118c33ba4b6af66b275eb8da8a45b87f629
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-28 16:04:38 -0300

    task2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 """pyhton interpreter"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at
+    the end of a text file (UTF8) and returns
+     the number of characters added"""
+    with open(filename, 'a') as f:
+        append_n = f.write(text)
+        return append_n
