8da758011227e1c5e6b25af34918e796452dfba2
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-28 15:54:51 -0300

    task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 """pyhton interpreter"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to
+    a text file (UTF8) and returns the
+    number of characters written"""
+    with open(filename, 'w', encoding="UTF8") as f:
+        write_file = f.write()
+        print(write_file, end="")
