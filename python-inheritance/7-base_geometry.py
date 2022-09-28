b8a4c6771965972a8a58a7f162049cb46f25b3f6
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-09-28 09:23:31 -0300

    task7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""pyhton interpreter"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise TypeError("<name> must be greater than 0")
