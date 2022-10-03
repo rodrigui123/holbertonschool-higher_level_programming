81cdfac7a2e42b3a6b933e115a9c12453cc049b3
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-03 13:59:16 -0300

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,3 +16,37 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        width = self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        height = self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        x = self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        y = self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
