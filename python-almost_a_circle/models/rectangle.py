3b5cb9687910b5cca0e858c71c859bf7e9dbc420
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-03 17:41:24 -0300

    preturn str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,7 +36,7 @@ class Rectangle(Base):
     def __str__(self):
         """return str"""
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
-        f"- {self.__width}/{self.__height}")
+        f" - {self.__width}/{self.__height}")
 
     @property
     def width(self):
