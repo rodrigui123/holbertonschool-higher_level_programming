3fda179da38f2aed601c5d6013ef206f490796e5
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-03 15:38:30 -0300

    validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,9 +25,9 @@ class Rectangle(Base):
         if height <= 0:
             raise ValueError("height must be > 0")
         if x < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         if y < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__width = width
         self.__height = height
         self.__x = x
