8a010dc5ba8950de2dff3fcdee879aedccd912e2
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-03 13:22:08 -0300

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,8 @@ class Rectangle(Base):
     """class: inherit rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None)
         """class constructor"""
+        super().__init__(id)
+
         self.__width = width
         self.__height = height
         self.__x = x
