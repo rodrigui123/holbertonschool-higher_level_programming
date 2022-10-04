7d42b84ecc48ceb07dd8f3730a96ba2928751a62
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-04 13:34:33 -0300

    square getter setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,21 @@ class Square(Rectangle):
     def __str__(self):
         return (f"[Square] ({self.id}) {self.x}/{self.y}"
                 f" - {self.width}")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
