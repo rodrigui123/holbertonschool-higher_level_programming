b19f0b3a50909b3cc5d7831294bb7848e2fc4764
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-04 11:07:36 -0300

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""python interpreter"""
+
+
+from models.rectangle import Rectangle
+"""import class: Rectangle"""
+
+class Square(Rectangle):
+    """class: Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(size, size, id, x, y)
+
+    def __str__(self):
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.size}")
