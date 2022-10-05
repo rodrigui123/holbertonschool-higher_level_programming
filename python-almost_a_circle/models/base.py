34adf9bf18fecb599ff783360878e68534d8ef0f
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-05 11:19:37 -0300

    base json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,6 +21,6 @@ class Base(list):
     @staticmethod
     def to_json_string(list_dictionaries):
         """Dictionary to JSON string"""
-        if len(list_dictionaries) is None or len(list_dictionaries) is 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
