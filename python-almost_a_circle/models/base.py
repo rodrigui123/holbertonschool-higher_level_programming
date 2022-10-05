1dd25411d73f79e2469d4532a0224edf1dc3be70
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-05 11:22:39 -0300

    base json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,8 +15,8 @@ class Base(list):
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
