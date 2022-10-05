b658e62f88de3afec222ff11b35a76c40006350b
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-10-05 14:40:04 -0300

    task16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,9 +28,9 @@ class Base(list):
     @staticmethod
     def save_to_file(cls, list_objs):
         """JSON string to file"""
-        newlist = {}
+        newlist = []
         if list_objs is not None:
             for obj in list_objs:
                 newlist.append(cls.to_dictionary())
-        with open(f"{cls.__name__}.json", 'w') as f:
+        with open(f"{cls.__name__}.json", "w") as f:
             f.write(cls.to_json_string(newlist))
