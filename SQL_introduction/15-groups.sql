7197d68812a031ceafb85da3148ce6f164ef35ff
Rodrigo <5093@holbertonschool.com>
2022-11-06 14:59:19 -0800

    fixing15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in second_table
-SELECT score, COUNT(score) as 'number' 
-FROM secon_table 
+SELECT score, COUNT(score) as 'number'
+FROM secon_table
 GROUP BY score
 ORDER BY number DESC
