d81f88e5b7eae7904b25634ac5a89b9cb5201c49
Rodrigo <5093@holbertonschool.com>
2022-11-06 17:39:16 -0800

    fixing6

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	state_id NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY (state_id) REFERENCES states (id),
+	FOREIGN KEY (state_id) REFERENCES states(id),
 );
