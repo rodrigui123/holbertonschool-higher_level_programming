4bb52e1b476ce27fb8b33ebdc8474a1fbfc1e1c5
Rodrigo <5093@holbertonschool.com>
2022-11-06 17:33:37 -0800

    task7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,2 +1,12 @@
 -- creates the database hbtn_0d_usa and the table cities
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id NOT NULL FOREIGN KEY REFERENCES states (id),
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
+);
+
