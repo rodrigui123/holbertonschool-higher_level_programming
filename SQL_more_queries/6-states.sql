d861cf2fc5c8332aa2c4e2142a842647d2114629
Rodrigo <5093@holbertonschool.com>
2022-11-06 17:07:40 -0800

    t6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- creates the database hbtn_0d_usa and the table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
-	id INT DEFAULT 1 UNIQUE PRIMARY KEY, 
+	id INT DEFAULT 1 UNIQUE NOT NULL PRIMARY KEY, 
 	name VARCHAR(256) NOT NULL
 );
