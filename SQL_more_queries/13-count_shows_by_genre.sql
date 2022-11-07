04ecce3cc528280863600c4364417f0985563bc4
Rodrigo <5093@holbertonschool.com>
2022-11-06 18:48:48 -0800

    t13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
 
 SELECT g.name AS genre,
-       COUNT() AS number_of_shows
-FROM tv_genres g
+       COUNT(*) AS number_of_shows
+SELECT FROM tv_genres g
 JOIN tv_show_genres sg
 ON g.id = sg.genre_id
 GROUP BY g.name
