0d9fee4789c1c8788cfad54117651fcbb95e829a
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-11-09 20:12:41 -0300

    task1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 """task 1"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
+    )
+    mycursor = db.cursor()
+    mycursor.execute("SELECT FROM states WHERE user LIKE 'N%';")
+    result = mycursor.fetchall()
+    for record in result:
+        if record [1][0] == "N":
+            print(record)
+    mycursor.close()
+    db.close()
