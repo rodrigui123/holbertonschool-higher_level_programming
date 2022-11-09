8ed50b362002c4c4a7a803a15d381fb5eb6e47e3
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-11-09 19:36:09 -0300

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        username=sys.argv[1],
-        password=sys.argv[2],
-        name=sys.argv[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     mycursor = db.cursor()
     mycursor.execute("SELECT * FROM states;")
