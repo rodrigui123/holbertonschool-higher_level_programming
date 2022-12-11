3f0a5e899a9402f4650c9a15db41d6d3aa9f821b
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-12-10 21:55:28 -0300

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const args = process.argv.slice(2).length;
 
-if (args.length === 0) {
-    console.log("No argument")
-} else if (args.length === 3) {
-    console.log("Argument found")
+if (args === 0) {
+    console.log("No argument");
+} else if (args === 1) {
+    console.log("Argument found");
 } else {
-    console.log("Arguments found")
+    console.log("Arguments found");
 }
