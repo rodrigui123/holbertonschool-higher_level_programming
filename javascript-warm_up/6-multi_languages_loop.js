34a86bf9a1e817490d667c15d16a27c4fad8dec4
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-12-10 22:51:25 -0300

    task6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const array = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'];
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
 
 for (const line of array) {
   console.log(line);
