524be72a2553f90ccb15c5e2dfc24a437300cd60
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-12-11 19:07:24 -0300

    task7java

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,10 +5,10 @@ const x = process.argv[2];
 // Check if x can be converted to an integer
 if (isNaN(x)) {
   // x is not a number
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   // Print "C is fun" x times
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
