223e3cc436f0d0c18075bc056986141957bcba89
root <5093@holbertonschool.com>
2022-12-11 18:12:52 -0800

    task1java

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* Exporting the class Rectangle. */
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
