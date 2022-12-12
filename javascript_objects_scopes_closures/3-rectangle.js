4f93a8368be9892323849390c8e396c785cf8208
root <5093@holbertonschool.com>
2022-12-11 18:31:57 -0800

    task3java

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,17 @@
-i
+#!/usr/bin/node
+
+/* Exporting the class Rectangle. */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
