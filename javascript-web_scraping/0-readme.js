94d68a650d0a416366b5e33aafc5ff1cb4c53c1f
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-12-15 14:08:29 -0300

    task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,11 +4,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
     return;
   }
-
   console.log(data);
 });
